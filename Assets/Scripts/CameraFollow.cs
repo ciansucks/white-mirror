c33c65200cf948ae993ae88c6883d3b6bcf8a72c
ciansucks <cian.smyth42@gmail.com>
2020-02-21 12:11:26 -0700

    Uploaded new project
    completely wiped old files, uploaded new HDRP project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 offset;
+    public float MoveSpeed = 0.05f;
+    public bool UseOffset = true;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (UseOffset)
+        {
+            offset = transform.position - Player.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.position = Vector3.Lerp(transform.position, Player.transform.position + offset, MoveSpeed);
+
+    }
+    
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public GameObject reflectionPlane;
+    private bool shifted = false;
+
+
+
+
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(characterController.isGrounded)
+        {
+            Vector3 movementVector = Vector3.zero;
+            movementVector.x = Input.GetAxis("Horizontal");
+            movementVector.z = Input.GetAxis("Vertical");
+
+            //  GetComponent<Rigidbody>().AddForce(movementVector * moveSpeed);
+
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetButtonDown("Reflect"))
+        {
+            Debug.Log("Player Position: " + transform.position);
+            reflect(reflectionPlane);
+            Debug.Log("New Player Position: " + transform.position);
+
+        }
+
+    }
+
+    void reflect(GameObject reflectionField)
+    {
+
+
+        float offset = reflectionField.transform.position.x - transform.position.x;
+        Debug.Log("Offset: " + offset);
+        Debug.Log("ReflectionField Position: " + reflectionField.transform.position);
+        
+        Vector3 newPosition = new Vector3(reflectionField.transform.position.x + offset, transform.position.y, transform.position.z);
+        Debug.Log("Setting Position: " + newPosition);
+
+        transform.position = newPosition;
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Entered trigger zone of " + collision.gameObject);
+        if (collision.gameObject.CompareTag("ReflectionField"))
+        {
+            reflectionPlane = collision.gameObject;
+        }
+    }
+}
