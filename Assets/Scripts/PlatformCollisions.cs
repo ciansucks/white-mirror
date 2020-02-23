9b1eec688a165c772f7ba4119153b30ad145b7f7
ANGELIQUE MAE ABAN <angeliqueaban@gmail.com>
2020-02-23 14:29:31 -0700

    Dimension Switch Whiteboxing
    -added second level whiteboxing

---
+++ Assets/Scripts/PlatformCollisions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCollisions : MonoBehaviour
+{
+    enum PlatformColor { Platform_Red, Platform_Blue };
+
+    private GameObject[] RedPlat;
+    private GameObject[] BluePlat;
+    /// <summary>
+    /// Called on ColorEvent.Invoke() in the PlayerInput class. 
+    /// </summary>
+    /// <param name="Player"></param>
+    private void Start()
+    {
+        RedPlat = GameObject.FindGameObjectsWithTag("Platform_Red");
+        BluePlat = GameObject.FindGameObjectsWithTag("Platform_Blue");
+
+        SetActivePlatforms();
+    }
+    public void SetActivePlatforms()
+    {
+        //true means the platform is a trigger and is inactive
+        bool rTrig = true;
+        bool bTrig = true;
+
+        //check which color player is
+        if (this.tag == "Player_Red")
+            rTrig = false;
+        else 
+            bTrig = false;
+
+
+
+
+        //if player passes through red, set red as tangible
+        if (RedPlat[0].GetComponent<BoxCollider>().isTrigger != rTrig)
+        {
+            foreach (GameObject o in RedPlat)
+            {
+                o.GetComponent<BoxCollider>().isTrigger = rTrig;
+                Renderer r = o.GetComponent<Renderer>();
+                Color newColor = r.material.color;
+                if (rTrig)
+                    newColor.a = .0f;
+                else
+                    newColor.a = .5f;
+
+                r.material.color = newColor;
+            }
+
+        }
+
+        if (BluePlat[0].GetComponent<BoxCollider>().isTrigger != bTrig)
+        {
+            foreach (GameObject o in BluePlat)
+            {
+                o.GetComponent<BoxCollider>().isTrigger = bTrig;
+                Renderer r = o.GetComponent<Renderer>();
+                Color newColor = r.material.color;
+                if (bTrig)
+                    newColor.a = .0f;
+                else
+                    newColor.a = .5f;
+
+                r.material.color = newColor;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerDimensionShifting.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class PlayerDimensionShifting : MonoBehaviour
+{
+
+    public Image PlatformBar;
+
+    public UnityEvent ColorSwapEvent;
+    public string playerColor;
+    private bool isGrounded, isJumping, colorPress, colorCollision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerColor.ToUpper() == "RED")
+            PlatformBar.color = Color.red;
+        else
+            PlatformBar.color = Color.blue;
+
+        this.tag = "Player_" + playerColor;
+
+        isGrounded = true;
+        colorCollision = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorPress = Input.GetKeyDown(KeyCode.G);
+        if (colorPress && !colorCollision)
+            SwitchColor();
+    }
+    /// <summary>
+    /// On keypress, change the character's color and send an event to update any enemies or players as needed
+    /// </summary>
+    private void SwitchColor()
+    {
+
+        if (PlatformBar.color == Color.red)
+        {
+            PlatformBar.color = Color.blue;
+            this.tag = "Player_Blue";
+        }
+        else
+        {
+            PlatformBar.color = Color.red;
+            this.tag = "Player_Red";
+        }
+
+        ColorSwapEvent.Invoke();
+        Debug.Log("Swapping color to:" + this.tag);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Platform_Red") || collision.collider.CompareTag("Platform_Green") || collision.collider.CompareTag("Platform_Blue"))
+        {
+            colorCollision = true;
+            Debug.Log("Touching platform");
+        }
+        if (collision.collider.CompareTag("Enemy_Red") || collision.collider.CompareTag("Enemy_Blue") || collision.collider.CompareTag("Enemy_Green"))
+        {
+
+
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy_Red") || collision.collider.CompareTag("Enemy_Blue") || collision.collider.CompareTag("Enemy_Green"))
+        {
+
+
+        }
+        isGrounded = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        colorCollision = false;
+        isGrounded = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Platform_Red") || other.CompareTag("Platform_Green") || other.CompareTag("Platform_Blue"))
+        {
+            colorCollision = true;
+        }
+        else if (other.CompareTag("LevelEnd"))
+        {
+            Invoke("ResetWin", .5f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        colorCollision = false;
+
+    }
+
+}
