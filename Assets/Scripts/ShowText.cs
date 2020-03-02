ea3d3bcf6a808b53e0bdcfdf8ef9d3597912f674
ANGELIQUE MAE ABAN <angeliqueaban@gmail.com>
2020-03-02 15:15:55 -0700

    Text Terminals
    Added objects to test with text terminals
    -Press z when in yellow cube to activate text
    -leaving cube will make text disappear
    TO DO: read from text file to update the Text.text attribute.

---
+++ Assets/Scripts/ShowText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShowText : MonoBehaviour
+{
+    public TextAsset file;
+    //public UnityEvent<TextAsset> changeText;
+    public UnityEvent changeText;
+    public UnityEvent resetText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Read"))
+        {
+            Debug.Log("Reading file");
+            changeText.Invoke();
+            //changeText.Invoke(file);
+        }
+          
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        resetText.Invoke();
+
+    }
+}
