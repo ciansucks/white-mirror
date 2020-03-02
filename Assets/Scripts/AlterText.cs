fa4b0b0198acb8fd0f7327201bb98d7ea09ca199
ANGELIQUE MAE ABAN <angeliqueaban@gmail.com>
2020-03-02 15:22:26 -0700

    Text Box from File
    Reading from Text Asset onto file
    -Make Pretty GUI
    -Make it scale to screen?

---
+++ Assets/Scripts/AlterText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlterText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponentInChildren<Text>().text = "Beep";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+   public void UpdateText(TextAsset file)
+    {
+        this.GetComponentInChildren<Text>().text = file.text;
+    }
+}
