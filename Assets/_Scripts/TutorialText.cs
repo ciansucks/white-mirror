8a0e22a2dcf99d8ab5001d97542094e90e75d595
ANGELIQUE MAE ABAN <angeliqueaban@gmail.com>
2020-04-02 00:02:33 -0600

    Tutorial text
    Added for Door

---
+++ Assets/_Scripts/TutorialText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour
+{
+    public TextAsset file;
+    public Canvas canvas;
+    private Image tutorialBG;
+    private Text tutorialTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialBG = canvas.transform.Find("TutorialPanel").GetComponent<Image>();
+        tutorialTxt = tutorialBG.GetComponentInChildren<Text>();
+        tutorialTxt.enabled = false;
+        tutorialBG.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+            tutorialTxt.text = file.text;
+            tutorialTxt.enabled = true;
+            tutorialBG.enabled = true;
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+            tutorialTxt.enabled = false;
+            tutorialBG.enabled = false;
+
+    }
+}
